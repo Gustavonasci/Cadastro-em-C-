e16aa2b81af9513f448fa4cf6ecb08b111494a31
Gustavo Nascimento da Silva <89107846+Gustavonasci@users.noreply.github.com>
2021-09-09 20:35:37 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Aula03
+{
+    class Program
+    {
+        static void Main()
+        {
+            string nome, cpf, end;
+            int cep, idade ;
+            Console.WriteLine("==============PREENCHA COM SEU DADOS AS SEGUINTES INFORMAÇÔES.==============");
+            Console.WriteLine("========================PREENCHA SEU NOME.==================================");
+            nome = Console.ReadLine();
+            Console.WriteLine("========================PREENCHA SEU CPF.===================================");
+            cpf = Console.ReadLine();
+            Console.WriteLine("========================PREENCHA SEU ENDEREÇO.==============================");
+            end = Console.ReadLine();
+            Console.WriteLine("========================PREENCHA SUA IDADE.=================================");
+            idade = Convert.ToInt32  (Console.ReadLine());
+            Console.WriteLine("========================PREENCHA SEU CEP.===================================");
+            cep = Convert.ToInt32 (Console.ReadLine());
+
+            Console.WriteLine($"==============SEU CADASTRO ESTÁ OK, CONFIRME AS INFORMAÇÔES=============");
+
+        
+            Console.WriteLine($"NOME COMPLETO: {nome}");
+
+            Console.WriteLine($"SEU CPF: {cpf}");
+
+            Console.WriteLine($"SUA IDADE É: {idade} ANOS");
+
+            Console.WriteLine($"SEU ENDEREÇO: {end}");
+
+            Console.WriteLine($"SEU CEP: {cep}");
+            
+
+        }
+    }
+}
